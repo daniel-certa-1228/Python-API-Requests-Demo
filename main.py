ceef627222b0f1305d43f7bd9e1c1cfda2b18497
Dan Certa <dcerta@oneeightycapital.com>
2018-11-10 19:58:11 -0600

    enhancing datetimes to allow for UTC offsets

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import weather_app
 import email_wx
-import time
+from datetime import datetime, timedelta
 import os
 
 weather = weather_app.recursiveFlatten(weather_app.getWeather())
 
 string = ""
 string += f"**{weather.get('name')} Weather**\n"
-string += f"Date: {time.strftime('%m-%d-%Y', time.localtime(weather.get('dt')))}\n"
-string += f"Time: {time.strftime('%H:%M', time.localtime(weather.get('dt')))}\n"
+string += f"Date: {datetime.fromtimestamp(weather.get('dt')).strftime('%m-%d-%Y')}\n"
+string += f"Time: {datetime.fromtimestamp(weather.get('dt')).strftime('%H:%M')}\n"
 string += f"Skies: {weather.get('main')}\n"
 string += f"Current Temp: {round(weather.get('temp'))}°F\n"
 string += f"Humidity: {weather.get('humidity')}%\n"
-string += f"Sunrise: {time.strftime('%H:%M', time.localtime(weather.get('sunrise')))}\n"
-string += f"Sunset: {time.strftime('%H:%M', time.localtime(weather.get('sunset')))}\n"
+string += f"Sunrise: {datetime.fromtimestamp(weather.get('sunrise')).strftime('%H:%M')}\n"
+string += f"Sunset: {datetime.fromtimestamp(weather.get('sunset')).strftime('%H:%M')}\n"
 
 print(string)
 
